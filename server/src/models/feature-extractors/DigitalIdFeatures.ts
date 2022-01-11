167819ef551ccbe532e9c336bb333107436da995
Evan Coulson <evan.m.coulson@gmail.com>
2022-01-10 20:18:54 -0800

    Planning text extraction service

--- server/src/models/feature-extractors/DigitalIdFeatures.ts
+++ server/src/models/feature-extractors/DigitalIdFeatures.ts
@@ -1,8 +1,9 @@
 import FileStream from "../common/files/FileStream";
+import TextPatternLookup from "../text-extractors/TextPatternLookup";
 
 export default class DigitalFeatures {
-    public readonly logoFileStream: FileStream;
-    public readonly faceFileStream: FileStream;
-    public readonly sourceFileStream: FileStream;
-    public readonly parsedTextPatterns: 
+	public readonly logoFileStream: FileStream;
+	public readonly faceFileStream: FileStream;
+	public readonly sourceFileStream: FileStream;
+	public readonly parsedTextPatterns: TextPatternLookup;
 }
